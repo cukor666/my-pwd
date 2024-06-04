8cd8b52f301e0217347ed26e5b5aa339bce06c30
Cukor <2450808861@qq.com>
2024-06-04 19:45:04 +0800

    初始化项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const title = ref('Welcome to my app')
+</script>
